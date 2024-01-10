0882adeb8d371f65f8a9c23415901198d1151172
Daniel <daniel@gmail.com>
2024-01-10 11:14:32 +0300

    filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+function that Define filter_datum & returns an obfuscated log message
+"""
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Obfuscate specified fields in the log message.
+
+    Arguments:
+        fields: List of strings representing fields to obfuscate.
+        redaction: String representing the obfuscation for the fields.
+        message: String representing the log line.
+        separator: String representing the character separating fields.
+
+    Returns:
+        String: Log message with specified fields obfuscated.
+    """
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
+    return message
+
+
+if __name__ == "__main__":
+    main()
