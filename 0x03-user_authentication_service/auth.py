7f5ae3abdee91dfa6e4be510b9202a7d0798a2f1
Daniel <daniel@gmail.com>
2024-01-23 16:15:08 +0300

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ Definition of _hash_password function
 import bcrypt
 from db import DB
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
