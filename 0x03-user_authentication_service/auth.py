fbac608c1280de3a335068b2341f2c1cb4f1b5df
Daniel <daniel@gmail.com>
2024-01-23 19:09:18 +0300

    Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
